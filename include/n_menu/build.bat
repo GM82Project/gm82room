74a6674d02df50734871073418b9dc5091ee72f2
omicronrex <65692652+omicronrex@users.noreply.github.com>
2024-04-02 00:15:33 -0300

    fix n_menu build script

--- include/n_menu/build.bat
+++ include/n_menu/build.bat
@@ -1,7 +1,7 @@
-call "%VS120COMNTOOLS%vsvars32.bat"
+call vcvars32.bat"
 
 cl N_Menu.cpp /O2 /GS- /nologo /link gdi32.lib user32.lib /nologo /dll /out:N_Menu.dll
 
-build_gex.py N_Menu.ged
+py gm82gex.py N_Menu.gej
 
 pause
